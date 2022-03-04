53887f1a500c31979645b46767ea3a48cabbe692
Atya <nushnushka@gmail.com>
2022-03-04 13:59:43 -0300

    gunicorn added

--- single_page_website/settings.py
+++ single_page_website/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 import os
 # import dj_database_url
-import django_heroku
 from pathlib import Path
 
 from django.conf.global_settings import DATABASES
