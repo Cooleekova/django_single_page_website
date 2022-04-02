1dcb6e34d6ebfafd976187b068688b124c311f75
Atya <nushnushka@gmail.com>
2022-04-02 10:46:41 -0300

    new migrations

--- single_page_website/settings.py
+++ single_page_website/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'single_page_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'single_page_website',
+        'NAME': 'list_of_films',
         'USER': user,
         'PASSWORD': password,
         'HOST': '127.0.0.1',
